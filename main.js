b911fd303960fef435559aecb7300457701dd98d
slava22822 <148676437+slava22822@users.noreply.github.com>
2025-03-29 14:53:50 +0400

    Update
    Что-то новое

---
+++ main.js
@@ -0,0 +1,14 @@
+const copytextelement = document.querySelectorAll('.batton-ip')
+    copytextelement.forEach(Element => {
+        Element.addEventListener('click',async() => {
+            const textcopy = Element.dataset.text;
+            
+            try{
+                await navigator.clipboard.writeText(textcopy)
+                console.log('Текст скопирован..')
+            } catch (err){
+                console.error('Не получилось скопировать текст!', err)
+            }
+        })
+    })
+ 
